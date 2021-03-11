d198b47b89dd47149f26afb3cdf0e0ee1ce6af9d
PatrickPoncet <patrick@poncet.ca>
2021-03-11 05:26:50 -0500

    finalize for automation

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import os
+import logging
+import sys
+
+sys.path.append("./agoraplus-quebec/python")
+
+import agoraplusmstranscribeautomated as pyscraper
 
 print("Running python script")
 print('------')
-print(os.environ.get("HUB_USERNAME"))
-print(os.environ.get("HUB_PASSWORD"))
+
+try: 
+    scriptname = 'agoraplusmstranscribeautomated.py'
+    filename = os.environ.get('LOG_PATH') + '/' + scriptname + '.log'
+    print('logging to ' + filename)
+    logging.basicConfig(level=logging.INFO, filename=filename, filemode='w', format='%(asctime)s : %(levelname)s - %(message)s',datefmt='%d-%b-%y %H:%M:%S')
+    logging.info('logging to ' + filename)
+    pyscraper.main()
+ 
+except Exception as e: 
+    print("An error occured : " + str(e))
+    logging.info("An error occured : " + str(e))
+
+else:
+    print("Python script finished successfuly")
+
+finally:
+    print("finally done")
