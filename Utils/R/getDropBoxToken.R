591e4cf9a95d162da3f233b2b87469bceab4c9ab
PatrickPoncet <patrick@poncet.ca>
2021-03-11 13:57:45 -0500

    added more comments + httpuv package

--- Utils/R/getDropBoxToken.R
+++ Utils/R/getDropBoxToken.R
@@ -1,7 +1,28 @@
 # Create My Dropbox Token
 
-# Run this first and follow the instructions
+# - Run this first and follow the instructions
+# - Essentially it will launch a browser on your PC
+# - From it, you will have to log in to your dropbox account
+#   and authorize the application to access your account.
+# - Once that is done copy/paste the token character string that
+#   will appear in your R console and put it in the .env file
+#   of your container (if you are running a container), or make it
+#   a globa environment variable on your system in order to run the
+#   python youtube extractor
+# - Also take the token.rds file that was created and saved bu this script
+#   on your hard drive and put it in the working directory of the R 
+#   script agoraplus-youtube.R.  And if you plan to run this in a container
+#   you must put the token file in the ./src directory of your container
+# - In your dropbox account there must a folder at the root 
+#   called clessn-blend (create it if it does not exist)
+#   In it you must put the shared folder _SharedFolder_clessn-blend
+#   (ask your CLESSN admin to share the folder to you)
+
 devtools::install_github("karthik/rdrop2")
+install.packages("httpuv")
+
+library(httpuv)
+library(rdrop2)
 
 token <- rdrop2::drop_auth()
 saveRDS(token, "token.rds")
