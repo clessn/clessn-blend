41956bbb7166aa3a8b6914a8a7c2036121bb80fe
Patrick Poncet <patrick@infoscope.ca>
2021-08-16 00:48:28 -0400

    Fixed nbsps

--- agoraplus-pressrelease/agoraplus-pressrelease.R
+++ agoraplus-pressrelease/agoraplus-pressrelease.R
@@ -102,15 +102,21 @@ if (plc_r$result$status_code == 200) {
       plc_comm_text <- XML::getNodeSet(plc_comm_root, ".//div[@class='post-content-container']")
       plc_comm_text <- XML::getNodeSet(plc_comm_text[[1]], ".//p")
       
-      plc_comm_location <- XML::xmlValue(XML::getNodeSet(plc_comm_text[[2]], "em"))
-      
       text <- ""
       
       for (i_plc_comm_text in 2:length(plc_comm_text)) {
         text <- paste(text, XML::xmlValue(plc_comm_text[[i_plc_comm_text]]), sep="\n\n")
       }
       
-      plc_comm_text <- text
+      plc_comm_text <- trimws(text)
+      plc_comm_text <- gsub("\u00a0", "", plc_comm_text)
+      
+      
+      # Récupère le lieu
+      plc_comm_location <- substr(plc_comm_text, 1, stringr::str_locate(plc_comm_text, "-|—|–")[1,1][[1]]-1)
+      plc_comm_location <- trimws(plc_comm_location)
+      plc_comm_location <- stringr::str_to_title(plc_comm_location)
+      if (nchar(plc_comm_location) > 25) plc_comm_location <- NA
       
       # Construit le data pour le hub
       key <- digest::digest(plc_url_list[[i_plc_url_list]])
@@ -118,7 +124,7 @@ if (plc_r$result$status_code == 200) {
       schema <- "v1"
       
       metadata <- list("date"=Sys.time(), "url"=plc_url_list[[i_plc_url_list]])
-      data <- list("date"=plc_comm_date, "place"=plc_comm_location, "content"=plc_comm_text)
+      data <- list("date"=plc_comm_date, "location"=plc_comm_location, "title"=plc_comm_title, "content"=plc_comm_text)
       
       hub_items <- NULL
       filter <- clessnhub::create_filter(key = key)
@@ -129,7 +135,7 @@ if (plc_r$result$status_code == 200) {
         clessnhub::create_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
       } else {
         # ce communiqué (avec cette key) existe dans le hub 
-        #clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
+        clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
         print("ce communiqué existe déjà")
       }
       
@@ -189,10 +195,6 @@ if (pcc_r$result$status_code == 200) {
       pcc_comm_title <- XML::xmlValue(pcc_comm_title)
       
       # Récupère la date
-      #pcc_comm_date <- XML::getNodeSet(pcc_comm_root, ".//div[@class='post-content']")
-      #pcc_comm_date <- XML::getNodeSet(pcc_comm_date[[1]], ".//p")
-      #pcc_comm_date <- XML::xmlValue(pcc_comm_date[[1]])
-      #pcc_comm_date <- gsub("POUR DIFFUSION IMMÉDIATE", "", pcc_comm_date)
       pcc_comm_date <- pcc_date_list[[i_pcc_url_list]]
 
       # Récupère le contenu
@@ -216,7 +218,9 @@ if (pcc_r$result$status_code == 200) {
         text <- paste(text, XML::xmlValue(pcc_comm_text[[i_pcc_comm_text]]), sep = "\n\n")
       }
       
-      pcc_comm_text <- text
+      pcc_comm_text <- trimws(text)
+      pcc_comm_text <- gsub("\u00a0", "", pcc_comm_text)
+      
       
       # Construit le data pour le hub
       key <- digest::digest(pcc_url_list[[i_pcc_url_list]])
@@ -224,7 +228,7 @@ if (pcc_r$result$status_code == 200) {
       schema <- "v1"
       
       metadata <- list("date"=Sys.time(), "url"=pcc_url_list[[i_pcc_url_list]])
-      data <- list("date"=pcc_comm_date, "place"=pcc_comm_location, "content"=pcc_comm_text)
+      data <- list("date"=pcc_comm_date, "Location"=pcc_comm_location, "title"=pcc_comm_title, "content"=pcc_comm_text)
       
       hub_items <- NULL
       filter <- clessnhub::create_filter(key = key)
@@ -235,7 +239,7 @@ if (pcc_r$result$status_code == 200) {
         clessnhub::create_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
       } else {
         # ce communiqué (avec cette key) existe dans le hub 
-        #clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
+        clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
         print("ce communiqué existe déjà")
       }
       
@@ -300,6 +304,9 @@ if (blq_r$result$status_code == 200) {
       # Récupère le texte
       blq_comm_text <- XML::getNodeSet(blq_comm_text[[1]], ".//p")
       blq_comm_text <- paste(XML::xmlValue(blq_comm_text), collapse = '\n\n')
+      blq_comm_text <- trimws(blq_comm_text)
+      blq_comm_text <- gsub("\u00a0", "", blq_comm_text)
+      
       
       # Construit le data pour le hub
       key <- digest::digest(blq_url_list[[i_blq_url_list]])
@@ -307,7 +314,7 @@ if (blq_r$result$status_code == 200) {
       schema <- "v1"
       
       metadata <- list("date"=Sys.time(), "url"=blq_url_list[[i_blq_url_list]])
-      data <- list("date"=blq_comm_date, "place"=blq_comm_location, "content"=blq_comm_text)
+      data <- list("date"=blq_comm_date, "location"=blq_comm_location, "title"=blq_comm_title, "content"=blq_comm_text)
       
       hub_items <- NULL
       filter <- clessnhub::create_filter(key = key)
@@ -318,7 +325,7 @@ if (blq_r$result$status_code == 200) {
         clessnhub::create_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
       } else {
         # ce communiqué (avec cette key) existe dans le hub 
-        #clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
+        clessnhub::edit_item('agoraplus_press_releases', key = key, type = type, schema = schema, metadata = metadata, data = data)
         print("ce communiqué existe déjà")
       }
       
@@ -377,6 +384,9 @@ if (grn_r$result$status_code == 200) {
       # Récupère le texte
       grn_comm_text <- XML::getNodeSet(grn_comm_text[[1]], ".//p")
       grn_comm_text <- paste(XML::xmlValue(grn_comm_text), collapse = '\n\n')
+      grn_comm_text <- trimws(grn_comm_text)
+      #grn_comm_text <- gsub("\u00a0", "", grn_comm_text)
+      
       
       grn_comm_location <- substr(grn_comm_text, 1, stringr::str_locate(grn_comm_text, "-|—")[1,1][[1]]-1)
       grn_comm_location <- trimws(grn_comm_location)
@@ -389,7 +399,7 @@ if (grn_r$result$status_code == 200) {
       schema <- "v1"
       
       metadata <- list("date"=Sys.time(), "url"=grn_url_list[[i_grn_url_list]])
-      data <- list("date"=grn_comm_date, "place"=grn_comm_location, "content"=grn_comm_text)
+      data <- list("date"=grn_comm_date, "location"=grn_comm_location, "title"= grn_comm_title, "content"=grn_comm_text)
       
       hub_items <- NULL
       filter <- clessnhub::create_filter(key = key)
@@ -447,21 +457,29 @@ if (npd_r$result$status_code == 200) {
       
       # Récupère le contenu
       npd_comm_text <- XML::getNodeSet(npd_comm_root, ".//div[@class='news2-holder news2-body article-text']")
+
       npd_comm_text <- XML::xpathApply(npd_comm_text[[1]], ".//p")
-      npd_comm_text <- XML::xmlValue(npd_comm_text)
-      npd_comm_text <- paste(npd_comm_text, collapse = '\n\n')
       
-      # Récupère la date
-      npd_comm_date <- XML::getNodeSet(npd_comm_root, ".//div[@class='news2-holder news2-date article-text']")[[1]]
-      npd_comm_date <- trimws(XML::xmlValue(npd_comm_date))
+      if (length(npd_comm_text) > 2) {
+        npd_comm_text <- paste(XML::xmlValue(npd_comm_text), collapse = '\n\n')
+      } else {
+        npd_comm_text <- XML::getNodeSet(npd_comm_root, ".//div[@class='news2-holder news2-body article-text']")
+        npd_comm_text <- XML::xmlValue(npd_comm_text)
+        npd_comm_text <- trimws(npd_comm_text)
+        npd_comm_text <- gsub("\t\t\t\t", "\n\n",npd_comm_text)
+        npd_comm_text <- gsub("\n\n\t", "\n\n",npd_comm_text)
+        npd_comm_text <- gsub("\n\t\t", "\n\n",npd_comm_text)
+      }
       
-      # Récupère le texte
-      
+      # Récupère l'endroit
       npd_comm_location <- substr(npd_comm_text, 1, stringr::str_locate(npd_comm_text, "-|—|–")[1,1][[1]]-1)
       npd_comm_location <- trimws(npd_comm_location)
       npd_comm_location <- stringr::str_to_title(npd_comm_location)
       if (nchar(npd_comm_location) > 25) npd_comm_location <- NA
       
+      # Récupère la date
+      npd_comm_date <- XML::getNodeSet(npd_comm_root, ".//div[@class='news2-holder news2-date article-text']")[[1]]
+      npd_comm_date <- trimws(XML::xmlValue(npd_comm_date))
       
       # Construit le data pour le hub
       key <- digest::digest(npd_url_list[[i_npd_url_list]])
@@ -469,7 +487,7 @@ if (npd_r$result$status_code == 200) {
       schema <- "v1"
       
       metadata <- list("date"=Sys.time(), "url"=npd_url_list[[i_npd_url_list]])
-      data <- list("date"=npd_comm_date, "place"=npd_comm_location, "content"=npd_comm_text)
+      data <- list("date"=npd_comm_date, "location"=npd_comm_location, "title"=npd_comm_title, "content"=npd_comm_text)
       
       hub_items <- NULL
       filter <- clessnhub::create_filter(key = key)
