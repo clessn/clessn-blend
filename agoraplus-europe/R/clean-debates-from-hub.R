363844bb8523b98c7c7c00cd9c4250fee8c306e6
Patrick Poncet <patrick@infoscope.ca>
2022-01-19 23:06:42 -0500

    new versions

--- agoraplus-europe/R/clean-debates-from-hub.R
+++ agoraplus-europe/R/clean-debates-from-hub.R
@@ -1,8 +1,116 @@
 clessnhub::connect_with_token(Sys.getenv('HUB_TOKEN'))
-metadata_filter <- list(location="EU")
-filter <- clessnhub::create_filter(type="parliament_debate", schema="v2", metadata=metadata_filter)  
-df <- clessnhub::get_items('agoraplus_interventions', filter=filter, download_data = TRUE)
+#metadata_filter <- list(location="EU")
+#filter <- clessnhub::create_filter(type="parliament_debate", schema="v2", metadata=metadata_filter)  
+#df <- clessnhub::get_items('agoraplus_interventions', filter=filter, download_data = TRUE)
 
-for (i in 1:nrow(df)) {
-  clessnhub::delete_item('agoraplus_interventions', df$key[i])
+# for (z in 1:nrow(df)) {
+# 
+#   if ( !is.null(df$data.interventionType[z]) && !is.na(df$data.interventionType[z]) && df$data.interventionType[z] != "" &&
+#        ((textcat::textcat(df$data.interventionType[z]) != "english" && textcat::textcat(df$data.interventionType[z]) != "scots") || 
+#         df$data.interventionType[z] == "moderation") ){
+#     
+#     if ( grepl("writing|written", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("blue-card", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("blue card", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("speech", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("for the", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("on behalf of", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("reporter", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("relator", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("on behalf of", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("mayor", tolower(df$data.interventionType[z])) ) next
+#     if ( grepl("lecturer", tolower(df$data.interventionType[z])) ) next
+#     
+#     origInterventionType <- df$data.interventionType[z]
+#     
+#     if ( tolower(df$data.interventionType[z]) == "allocution" || tolower(df$data.interventionType[z]) == "moderation" ) {
+#       tr <- "Speech"
+#     } else if ( tolower(df$data.interventionType[z]) == "par écrit" || tolower(df$data.interventionType[z]) == "écrit" || tolower(df$data.interventionType[z]) == "písemnĕ" ) {
+#       tr <- "In Writing"
+#     } else if ( tolower(df$data.interventionType[z]) == "rapporteur" || tolower(df$data.interventionType[z]) == "protractor" ) {
+#       tr <- "Reporter"
+#     } else {
+#       tr <- clessnverse::translateText(df$data.interventionType[z], engine="azure", target_lang="en",fake=F)[2]
+#     }
+# 
+#     if ( !is.null(tr) && !is.na(tr) ) {
+#       tr <- gsub("\\(", "", tr)
+#       tr <- gsub("\\)", "", tr)
+#       tr <- gsub("\"", "", tr)
+#       tr <- gsub("\\\\u2012", "", tr)
+#       tr <- gsub("\\\\ U2012", "", tr)
+#       tr <- stringr::str_squish(tr)
+#       tr <- trimws(tr)
+#       tr <- stringr::str_to_title(tr)
+#       
+#       df$data.interventionType[z] <- tr
+#     }
+#     
+#     df$data.interventionText[z] <- gsub("^NA\n\n", "", df$data.interventionText[z])
+#     df$data.interventionText[z] <- gsub("^\n\n", "", df$data.interventionText[z])
+#     df$data.interventionTextEN[z] <- gsub("^NA\n\n", "", df$data.interventionTextEN[z])
+#     df$data.interventionTextEN[z] <- gsub("^\n\n", "", df$data.interventionTextEN[z])
+#     
+#     interv_metadata <- df[z,which(stringr::str_detect(colnames(df), "^metadata."))]
+#     names(interv_metadata) <- gsub("metadata.", "", names(interv_metadata))
+#     interv_data <- df[z,which(stringr::str_detect(colnames(df), "^data."))]
+#     names(interv_data) <- gsub("data.", "", names(interv_data))
+#     
+#     
+#     # cat(df$key[z],df$type[z],df$schema[z],"\n\n")
+#     # print(as.list(interv_metadata))
+#     # print(as.list(interv_data))
+#     # cat("\n\n")
+#     cat(z, df$key[z], origInterventionType, df$data.interventionType[z], "\n")
+#     
+#     clessnhub::edit_item('agoraplus_interventions', df$key[z], df$type[z], df$schema[z], as.list(interv_metadata), as.list(interv_data))
+#     
+#   }
+# }
+
+for (z in 1:nrow(df)) {
+
+    origInterventionType <- df$data.interventionType[z]
+    
+    if (is.null(origInterventionType) || is.na(origInterventionType) || origInterventionType == "NA") next
+
+    tr <- dfInterventions$data.interventionType[z]
+      
+    if ( !is.null(tr) && !is.na(tr) ) {
+      tr <- gsub("\\(", "", tr)
+      tr <- gsub("\\)", "", tr)
+      tr <- gsub("\"", "", tr)
+      tr <- gsub("\\\\u2012", "", tr)
+      tr <- gsub("\\\\ U2012", "", tr)
+      tr <- gsub("\\\\", "", tr)
+      tr <- stringr::str_squish(tr)
+      tr <- trimws(tr)
+      tr <- stringr::str_to_title(tr)
+      
+      if ( grepl("in writing", tolower(tr)) || grepl("par écrit", tolower(tr)) ) tr <- "In Writing"
+
+      df$data.interventionType[z] <- tr
+    }
+
+    df$data.interventionText[z] <- gsub("^NA\n\n", "", df$data.interventionText[z])
+    df$data.interventionText[z] <- gsub("^\n\n", "", df$data.interventionText[z])
+    df$data.interventionTextEN[z] <- gsub("^NA\n\n", "", df$data.interventionTextEN[z])
+    df$data.interventionTextEN[z] <- gsub("^\n\n", "", df$data.interventionTextEN[z])
+
+    interv_metadata <- df[z,which(stringr::str_detect(colnames(df), "^metadata."))]
+    names(interv_metadata) <- gsub("metadata.", "", names(interv_metadata))
+    interv_data <- df[z,which(stringr::str_detect(colnames(df), "^data."))]
+    names(interv_data) <- gsub("data.", "", names(interv_data))
+
+
+    # cat(df$key[z],df$type[z],df$schema[z],"\n\n")
+    # print(as.list(interv_metadata))
+    # print(as.list(interv_data))
+    # cat("\n\n")
+    cat(z, df$key[z], origInterventionType, df$data.interventionType[z], "\n")
+
+    clessnhub::edit_item('agoraplus_interventions', df$key[z], df$type[z], df$schema[z], as.list(interv_metadata), as.list(interv_data))
+
+  
 }
+
--- agoraplus-europe/R/clean-mps-from-hub.R
+++ agoraplus-europe/R/clean-mps-from-hub.R
@@ -3,35 +3,139 @@ is.na.char <- function(x) {
 }
 
 clessnhub::connect_with_token(Sys.getenv('HUB_TOKEN'))
-filter <- clessnhub::create_filter(type="mp", schema="v3")  
-df <- clessnhub::get_items('persons', filter=filter, download_data = TRUE)
+filter <- clessnhub::create_filter(type="mp", schema="v3", metadata=list(institution="European Parliament"))  
+dfPersonsDirty <- clessnhub::get_items('persons', filter=filter, download_data = TRUE)
 
 n <- 1
 
-for (i in 1:nrow(df)) {
+
+###  Clean all names longer than 3 words
+# keys <- which(sapply(strsplit(df$data.fullName, " "),length) > 3)
+# 
+# fullnames <- df$data.fullName[which(sapply(strsplit(df$data.fullName, " "),length) > 3)]
+# 
+# for (u in 1:length(keys)) {
+#   clessnhub::delete_item('persons', keys[u])
+# }
+
+### Delete all european mps from hub
+# for (i in 1:nrow(df)) {
+#   clessnhub::delete_item('persons', df$key[i])
+# }
+
+### Delete all european mps of which the native name is not in the intervention dataset
+# for (i in 1:nrow(dfPersonsDirty)) {
+#   
+#   index <- which(dfInterventions_new2$data.speakerFullName == dfPersonsDirty$data.fullName[i])
+#   
+#   if ( length(index) == 0 ) {
+#     cat("deleting", dfPersonsDirty$data.fullName[i], "from hub\n")
+#     #clessnhub::delete_item('persons', df$key[i])
+#   }
+#}
+
+
+### Add missing traslated MP named to the hub from a dataset and correct existing ones
+clessnhub::connect_with_token(Sys.getenv('HUB_TOKEN'))
+
+logger <- clessnverse::loginit("clesn-mos-from-hub", "file,console", Sys.getenv("LOG_PATH"))
+
+df <- readRDS("dfPersonsToHub.rds")
+for (i in s:nrow(df)) {
+
+  # see if record with native name exists in the hub
+  if (!stringr::str_detect(df$data.fullNameNative[i], ":")) {
+    filter <- clessnhub::create_filter(type="mp", schema="v3", metadata=list(institution="European Parliament"), data=list(fullNameNative=df$data.fullNameNative[i]))  
+    person <- clessnhub::get_items('persons', filter=filter, download_data = TRUE)
+  } else {
+    clessnverse::logit("clean-mps-from-hub", paste("\n\n\n\n\n", i), logger)
+    clessnverse::logit("clean-mps-from-hub", paste("Skipping", df$data.fullName[i], "-", df$data.fullNameNative[i], "\n" ), logger)
+    next
+  }
   
-  clessnhub::delete_item('persons', df$key[i])
+  if (is.null(person)) {
+    
+    if (df$data.fullName[i] != "DeleteDelete") {
+      # Insert
+      clessnverse::logit("clean-mps-from-hub",paste("\n\n\n\n\n", i),logger)
+      clessnverse::logit("clean-mps-from-hub",paste("corrected version:",df$data.fullName[i], "-", df$data.fullNameNative[i]), logger)
+      clessnverse::logit("clean-mps-from-hub",paste("hub       version:",person$data.fullName, "-", person$data.fullNameNative), logger)
+      clessnverse::logit("clean-mps-from-hub",paste("inserting new mp",df$data.fullName[i], "-", df$data.fullNameNative[i]), logger)
+      new_person_metadata <- list("source"="https://www.europarl.europa.eu/meps/fr/download/advanced/xml?name=",
+                                  "country"=df$metadata.country[i],
+                                  "institution"="European Parliament",
+                                  "province_or_state"=df$metadata.country[i],
+                                  "twitterAccountHasBeenScraped"="0"
+                                  )
+      new_person_data <- list("fullName"=df$data.fullName[i],
+                              "fullNameNative" = df$data.fullNameNative[i],
+                              "isFemale"= df$data.isFemale[i],
+                              "lastName"=df$data.lastName[i],
+                              "firstName"=df$data.firstName[i],
+                              "twitterID"=NA_character_,
+                              "isMinister"="0",
+                              "twitterName"=NA_character_,
+                              "currentParty"=df$data.currentParty[i],
+                              "twitterHandle"=NA_character_,
+                              "currentMinister"=NA_character_,
+                              "currentPolGroup"=df$data.currentPolGroup[i],
+                              "twitterLocation"=NA_character_,
+                              "twitterPostsCount"=NA_character_,
+                              "twitterProfileURL"=NA_character_,
+                              "twitterListedCount"=NA_character_,
+                              "twitterFriendsCount"=NA_character_,
+                              "currentFunctionsList"=NA_character_,
+                              "twitterFollowersCount"=NA_character_,
+                              "currentProvinceOrState"=df$data.currentProvinceOrState[i],
+                              "twitterAccountVerified"=NA_character_,
+                              "twitterProfileImageURL"=NA_character_,
+                              "twitterAccountCreatedAt"=NA_character_,
+                              "twitterAccountCreatedOn"=NA_character_,
+                              "twitterAccountProtected"=NA_character_,
+                              "twitterProfileBannerURL"=NA_character_,
+                              "twitterUpdateDateStamps"=NA_character_,
+                              "twitterUpdateTimeStamps"=NA_character_
+                              )
+      
+      clessnhub::create_item(table = 'persons', key = paste("EU-",digest::digest(new_person_data$fullNameNative),sep=''), 
+                             type = "mp", schema = "v3", metadata = new_person_metadata, data = new_person_data)
+      next
+    } 
+    
+  } else {
   
-  # if (!is.na(df$data.currentFunctionsList[i])) {
-  #   cat(n, " found ", i, df$key[i], "\n\n")
-  #   n <- n + 1
-  #   
-  #   data_to_commit <- as.list(df[i,which(grepl("^data.",names(df[i,])))])
-  #   names(data_to_commit) <- gsub("^data.", "", names(data_to_commit))
-  #   metadata_to_commit <- as.list(df[i,which(grepl("^metadata.",names(df[i,])))])
-  #   names(metadata_to_commit) <- gsub("^metadata.", "", names(metadata_to_commit))
-  #   
-  #   data_to_commit$currentFunctionsList <- NA_character_
-  #   
-  #   data_to_commit[sapply(data_to_commit,is.null)] <- NA_character_ 
-  #   metadata_to_commit[sapply(metadata_to_commit,is.null)] <- NA_character_
-  #   data_to_commit[sapply(data_to_commit,is.na)] <- NA_character_ 
-  #   metadata_to_commit[sapply(metadata_to_commit,is.na)] <- NA_character_
-  #   data_to_commit[sapply(data_to_commit,is.na.char)] <- NA_character_ 
-  #   metadata_to_commit[sapply(metadata_to_commit,is.na.char)] <- NA_character_
-  #   
-  #   clessnhub::edit_item('persons', key = df$key[i], type = df$type[i], schema = df$schema[i], metadata = metadata_to_commit, data = data_to_commit)
-  #   
-  # }
-  
+    if (nrow(person) > 1) stop("Wooo more than one row")
+    
+    if (df$data.fullName[i] == person$data.fullName && df$data.fullNameNative[i] == person$data.fullNameNative && person$data.fullNameNative == person$data.fullNameNative) {
+      # Doing nothing
+    } else {
+      # Doing something
+      clessnverse::logit("clean-mps-from-hub", paste("\n\n\n\n\n", i), logger)
+      clessnverse::logit("clean-mps-from-hub",paste("corrected version:",df$data.fullName[i], "-", df$data.fullNameNative[i]), logger)
+      clessnverse::logit("clean-mps-from-hub",paste("hub       version:",person$data.fullName, "-", person$data.fullNameNative), logger)
+
+      if (df$data.fullName[i] == "DeleteDelete") {
+        # Delete
+        clessnverse::logit("clean-mps-from-hub", paste("deleting", person$data.fullName, "-", person$data.fullNameNative), logger)
+        clessnhub::delete_item('persons', person$key)
+        next
+      }
+      
+      if (df$data.fullName[i] != person$data.fullName && df$data.fullNameNative[i] == person$data.fullNameNative) {
+        # Update
+        clessnverse::logit("clean-mps-from-hub", paste("updating",  person$data.fullName, "-", person$data.fullNameNative, "to", df$data.fullName[i], "in the hub"), logger)
+        person$data.fullName <- df$data.fullName[i]
+        person$data.firstName <- trimws(stringr::str_to_title(stringr::str_split(person$data.fullName, "\\s")[[1]][[1]]))
+        person$data.lastName <- trimws(stringr::str_to_title(stringr::str_match(person$data.fullName, paste("^",person$data.firstName,"(.*)$",sep=''))[2]))
+        person_metadata <- person[,which(stringr::str_detect(colnames(person), "^metadata."))]
+        names(person_metadata) <- gsub("metadata.", "", names(person_metadata))
+        person_data <- person[,which(stringr::str_detect(colnames(person), "^data."))]
+        names(person_data) <- gsub("data.", "", names(person_data))
+        clessnhub::edit_item('persons', person$key, person$type, person$schema, as.list(person_metadata), as.list(person_data))
+        next
+      }
+    }
+    
+  }
 }
+ 
