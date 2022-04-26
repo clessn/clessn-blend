e7c441a5f72ab8637ef0e97ed6b5dfef976f7567
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2022-04-26 16:51:50 -0400

    more tests

--- Utils/R/Misc/getAgoraPlusInterventionsFromHub2.0.R
+++ Utils/R/Misc/getAgoraPlusInterventionsFromHub2.0.R
@@ -12,8 +12,21 @@ filter <- clessnhub::create_filter(data=list("eventID"="920220406EN"))
 df <- df %>% bind_rows(clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T))
 
 
+filter <- clessnhub::create_filter(data=list("eventID"="441057HAN057"))
+df <- clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T)
+filter <- clessnhub::create_filter(data=list("eventID"="441056HAN056"))
+df <- df %>% bind_rows(clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T))
+filter <- clessnhub::create_filter(data=list("eventID"="441055HAN055"))
+df <- df %>% bind_rows(clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T))
+filter <- clessnhub::create_filter(data=list("eventID"="441055HAN054"))
+df <- df %>% bind_rows(clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T))
+filter <- clessnhub::create_filter(data=list("eventID"="441055HAN053"))
+df <- df %>% bind_rows(clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T))
 
-for (i in 1:nrow(df)) {
-    clessnhub::delete_item(table='agoraplus_interventions', key = df$key[i])
-}
 
+filter <- clessnhub::create_filter(metadata=list("location"="EU"), data=list(eventDate__gte="2022-04-01"))
+df <- clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T)
+
+
+filter <- clessnhub::create_filter(metadata=list("institution"="House of Commons of Canada"), data=list(eventDate__gte="2022-04-01"))
+df <- clessnhub::get_items(table = 'agoraplus_interventions', filter = filter, download_data = T)
