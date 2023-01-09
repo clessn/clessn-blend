f926049428388fc98e5cea3c74f196eb647fd240
Patrick Poncet <72944649+PatrickPoncet@users.noreply.github.com>
2023-01-09 07:48:57 -0500

    minor mods

--- utils/del_canada_parliament_debates.R
+++ utils/del_canada_parliament_debates.R
@@ -20,7 +20,7 @@ my_filter <- clessnhub::create_filter(
     institution="House of Commons of Canada", 
     format="xml"),
   data=list(
-    eventID="432123HAN123"
+    #eventID="432123HAN123"
   )
 )
 
