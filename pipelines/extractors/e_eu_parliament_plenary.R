0a05ca1d38ba1baccc66d5d65bea93296def3120
p2xcode <p2x@p2xco.de>
2023-02-06 20:13:23 +0000

    update template and commit new extractor for agora+ europe

---
+++ pipelines/extractors/e_eu_parliament_plenary.R
@@ -0,0 +1,260 @@
+###############################################################################
+################         Script Definitions and Specs        ##################
+###############################################################################
+#                                                                             
+#                                                                             
+#                          e_eu_parliament_plenary                        
+#                                                                             
+# This script extracts HTML pages from the plenary sessions of the European
+# parliament and stores them in the CLESSN data lake
+#                                                                             
+###############################################################################
+
+###############################################################################
+########################      Functions and Globals      ######################
+###############################################################################
+
+my_global_variable <- "my_value"
+
+extract_debates <- function(url_list) {
+
+  for (url in url_list) {
+    event_id <- stringr::str_match(url, "\\CRE-(.*)\\.")[2]
+    event_id <- stringr::str_replace_all(event_id, "[[:punct:]]", "")
+
+    metadata <- list(
+      format = "",
+      tags = "parliament,europe,agora+,european_parliament",
+      pillar = "agora+",
+      source = url,
+      description = "european parliament plenary session debate transcription",
+      object_type = "raw_data",
+      source_type = "website",
+      contant_type = "parliament_debate_transcription",
+      storage_class = "lake",
+      institution = "european_parliament"
+    )
+
+    clessnverse::logit(scriptname, paste("grabbing", url), logger)
+
+    r <- rvest::session(url)
+
+    if (r$response$status_code == 200) {
+      clessnverse::logit(scriptname, "successful get", logger)
+
+      if (grepl("text/html", r$response$headers$`content-type`)) {
+        metadata$format <- "html"
+        doc <- httr::content(r$response, as = 'text')
+      }
+
+      if (grepl("text/xml", r$response$headers$`content-type`)) {
+        metadata$format <- "xml"
+        doc <- httr::content(r$response, as = 'text')
+      }
+
+      clessnverse::logit(scriptname, paste("pushing", event_id), logger)
+
+      r <- clessnverse::commit_lake_item(
+        data = list(
+          key = event_id,
+          path = "agoraplus-europe",
+          item = doc
+        ),
+        metadata = metadata,
+        mode = "newonly",
+        credentials
+      )
+
+      if (r) {
+        clessnverse::logit(scriptname, paste("successfuly pushed debate", event_id, "to datalake"), logger)
+        harvested <<- harvested + 1
+      } else {
+        clessnverse::logit(scriptname, paste("error while pushing debate", event_id, "to datalake"), logger)
+      }
+
+    } else {
+      clessnverse::logit(scriptname, paste("there was an error getting", url), logger)
+      warning(paste("could not read", url))
+      next
+    }
+
+  }#</while>
+
+} #</my_function>
+
+
+
+###############################################################################
+########################               Main              ######################
+###############################################################################
+
+main <- function() {
+  base_url <- "https://www.europarl.europa.eu"
+
+  clessnverse::logit(scriptname, "starting main function", logger)
+  clessnverse::logit(scriptname, "parsing options", logger)
+
+  if(length(opt$scraping_option) == 1 && grepl(",", opt$scraping_option)) {
+    # The option parameter given to the script is multivalued - parse
+    opt$scraping_option <- trimws(strsplit(opt$scraping_option, ",")[[1]])
+  }
+
+  scraping_method<- opt$scraping_option[1]
+  start_date <- opt$scraping_option[2]
+  num_days <- as.integer(opt$scraping_option[3])
+  start_parliament <- as.integer(opt$scraping_option[4])
+  num_parliaments <- as.integer(opt$scraping_option[5])
+
+  clessnverse::logit(
+    scriptname, 
+    paste(
+      "scraping method is ", scraping_method,
+      " with start_date=", start_date,
+      " num_days=", num_days,
+      " start_parliament=", start_parliament,
+      " num_parliaments=", num_parliaments,
+      sep=""
+    ),
+    logger
+  )
+
+  if (scraping_method == "frontpage") {
+    content_url <- "/plenary/en/debates-video.html#sidesForm"
+    
+    source_page <- xml2::read_html(paste(base_url,content_url,sep=""))
+    source_page_html <- htmlParse(source_page, useInternalNodes = TRUE)
+    
+    urls <- rvest::html_nodes(source_page, 'a')
+    urls <- urls[grep("\\.xml", urls)]
+    urls <- urls[grep("https", urls)]
+    
+    urls_list <- rvest::html_attr(urls, 'href')
+    urls_list <- as.list(urls_list)
+  } else {
+    if (scraping_method == "range") {
+      date_vect <- seq( as.Date(start_date), by=1, len=num_days)
+      
+      urls_list <- list()
+      nb_deb <- 0
+      
+      for (d in date_vect) {
+        for (p in c(start_parliament:(start_parliament+num_parliaments-1))) {
+          url <- paste(base_url, "/doceo/document/CRE-", p, "-", as.character(as.Date(d, "1970-01-01")), "_EN.html",sep= '')
+          clessnverse::logit(scriptname, paste("trying", url), logger)
+          r <- httr::GET(url)
+          if (r$status_code == 200) {
+            urls_list <- c(urls_list, url)
+            nb_deb <- nb_deb + 1
+          }
+        }
+      }
+    } else {
+      clessnverse::logit(scriptname, paste("invalid scraping_option", scraping_method), logger)
+    }
+  }
+  
+  clessnverse::logit(
+    scriptname = scriptname,
+    message = paste("list of urls containing", length(urls_list), "debates"), 
+    logger = logger
+  )
+
+  extract_debates(urls_list)
+
+}
+
+
+
+tryCatch( 
+  withCallingHandlers(
+  {
+    library(dplyr)
+    
+    status <<- 0
+    final_message <<- ""
+    harvested <<- 0
+
+    if (!exists("scriptname")) scriptname <<- "e_eu_parliament_plenary"
+
+    # valid options for this script are
+    #    log_output = c("file","console","hub")
+    #    scrapind_method = c("range", "start_date", num_days, start_parliament, num_parliament) | "frontpage" (default)
+    #    hub_mode = "refresh" | "skip" | "update"
+    #    translate = "TRUE" | "FALSE"
+    #    refresh_data = "TRUE" | "FALSE"
+    #    
+    #    you can use log_output = c("console") to debug your script if you want
+    #    but set it to c("file") before putting in automated containerized production
+
+    #opt <<- list(
+    #    log_output = c("console"),
+    #    scraping_method = c("date_range", "2014-09-18", 1, 8, 1),
+    #    hub_mode = "refresh"
+    #)
+
+    if (!exists("opt")) {
+      opt <<- clessnverse::process_command_line_options()
+    }
+    
+    if (!exists("logger") || is.null(logger) || logger == 0) {
+      logger <<- clessnverse::log_init(scriptname, opt$log_output, Sys.getenv("LOG_PATH"))
+    }
+
+    clessnverse::logit(
+      scriptname, 
+      paste("options are", paste(names(opt), opt, collapse=" ", sep="=")),
+      logger
+    )
+    
+    # login to hublot
+    clessnverse::logit(scriptname, "connecting to hub", logger)
+
+    # connect to hublot
+    credentials <<- hublot::get_credentials(
+      Sys.getenv("HUB3_URL"), 
+      Sys.getenv("HUB3_USERNAME"), 
+      Sys.getenv("HUB3_PASSWORD"))
+    
+    # or connect to hub2
+    clessnhub::login(
+      Sys.getenv("HUB_USERNAME"),
+      Sys.getenv("HUB_PASSWORD"),
+      Sys.getenv("HUB_URL"))
+
+    clessnverse::logit(scriptname, paste("Execution of",  scriptname,"starting"), logger)
+    
+    main()
+  },
+
+  warning = function(w) {
+    clessnverse::logit(scriptname, paste(w, collapse=' '), logger)
+    print(w)
+    final_message <<- if (final_message == "") w else paste(final_message, "\n", w, sep="")    
+    status <<- 2
+  }),
+  
+  error = function(e) {
+    clessnverse::logit(scriptname, paste(e, collapse=' '), logger)
+    print(e)
+    final_message <<- if (final_message == "") e else paste(final_message, "\n", e, sep="")    
+    status <<- 1
+  },
+
+  finally={
+    clessnverse::logit(scriptname, final_message, logger)
+
+    clessnverse::logit(scriptname, 
+      paste(
+        harvested, 
+        "debates were harvested from the european parliament website"
+      ),
+      logger
+    )
+
+    clessnverse::logit(scriptname, paste("Execution of",  scriptname,"program terminated"), logger)
+    clessnverse::log_close(logger)
+    if (exists("logger")) rm(logger)
+    print(paste("exiting with status", status))
+    quit(status = status)
+  }
+)
--- pipelines/extractors/e_template_extractor_name.R
+++ pipelines/extractors/e_template_extractor_name.R
@@ -28,6 +28,8 @@ my_function <- function(parm1 = "default_value", parm2 = "defaul_value", parm3..
         {
             clessnverse::logit(scriptname, "doing this", logger)
             # some code that does stuff
+						some_variable_to_count_what_the_script_does <- some_variable_to_count_what_the_script_does + 1
+
         },
         error = function(e) {
             clessnverse::logit(scriptname, "give details about what went wrong", logger)
@@ -48,7 +50,6 @@ my_function <- function(parm1 = "default_value", parm2 = "defaul_value", parm3..
             }
         },
         finally = {
-            i_get_attempt <- i_get_attempt + 1
         }
         )
     }#</while>
@@ -72,6 +73,7 @@ main <- function(scriptname, logger, credentials) {
     # put some trycatch in your code that
     # set status and
     # append final_message
+		# call functions in your code
 
   }#</for (i in 1:10)>
   
@@ -80,77 +82,89 @@ main <- function(scriptname, logger, credentials) {
 
 
 tryCatch( 
-    withCallingHandlers(
-    {
-        library(dplyr)
-        
-        if (!exists("scriptname")) scriptname <<- "the_name_of_the_script_without_R_extension"
+	withCallingHandlers(
+	{
+		library(dplyr)
+		
+		status <<- 0
+		final_message <<- ""
+		some_variable_to_count_what_the_script_does <<- 0
+		
+		if (!exists("scriptname")) scriptname <<- "the_name_of_the_script_without_R_extension"
 
-        # you can use log_output = c("console") to debug your script if you want
-        # but set it to c("file") before putting in automated containerized production
-        opt <<- list(log_output = c("file"), hub_mode = "refresh")
+    # valid options for this script are
+    #    log_output = c("file","console","hub")
+    #    scrapind_method = c("range", "start_date", num_days, start_parliament, num_parliament) | "frontpage" (default)
+    #    hub_mode = "refresh" | "skip" | "update"
+    #    translate = "TRUE" | "FALSE"
+    #    refresh_data = "TRUE" | "FALSE"
+    #    
+    #    you can use log_output = c("console") to debug your script if you want
+    #    but set it to c("file") before putting in automated containerized production
 
-        if (!exists("opt")) {
-            opt <<- clessnverse::processCommandLineOptions()
-        }
+    #opt <<- list(
+    #    log_output = c("console"),
+    #    scraping_method = c("date_range", "2014-09-18", 1, 8, 1),
+    #    hub_mode = "refresh"
+    #)
 
-        if (!exists("logger") || is.null(logger) || logger == 0) logger <<- clessnverse::log_init(scriptname, opt$log_output, Sys.getenv("LOG_PATH"))
-        
-        # login to hublot
-        clessnverse::logit(scriptname, "connecting to hub", logger)
+		if (!exists("opt")) {
+				opt <<- clessnverse::process_command_line_options()
+		}
 
-        # connect to hublot
-        credentials <- hublot::get_credentials(
-            Sys.getenv("HUB3_URL"), 
-            Sys.getenv("HUB3_USERNAME"), 
-            Sys.getenv("HUB3_PASSWORD"))
-        
-        # or connect to hub2
-        #clessnhub::login(
-        #    Sys.getenv("HUB_USERNAME"),
-        #    Sys.getenv("HUB_PASSWORD"),
-        #    Sys.getenv("HUB_URL"))
-
-        clessnverse::logit(scriptname, paste("Execution of",  scriptname,"starting"), logger)
-
-        status <<- 0
-        final_message <<- ""
-        total_press_releases_scraped <<- 0
-        
-        main(scriptname, logger, credentials)
-    },
-
-    warning = function(w) {
-        clessnverse::logit(scriptname, paste(w, collapse=' '), logger)
-        print(w)
-        final_message <<- if (final_message == "") w else paste(final_message, "\n", w, sep="")    
-        status <<- 2
-    }),
-    
-    error = function(e) {
-        clessnverse::logit(scriptname, paste(e, collapse=' '), logger)
-        print(e)
-        final_message <<- if (final_message == "") e else paste(final_message, "\n", e, sep="")    
-        status <<- 1
-    },
-  
-    finally={
-        clessnverse::logit(scriptname, final_message, logger)
-
-        clessnverse::logit(scriptname, 
-            paste(
-                some_global_variable_to_count_stuff_your_script_has_done, 
-                "QC press releases were extracted from the web"
-            ),
-            logger
-        )
-
-        clessnverse::logit(scriptname, paste("Execution of",  scriptname,"program terminated"), logger)
-        clessnverse::log_close(logger)
-        rm(logger)
-        print(paste("exiting with status", status))
-        quit(status = status)
+    if (!exists("logger") || is.null(logger) || logger == 0) {
+      logger <<- clessnverse::log_init(scriptname, opt$log_output, Sys.getenv("LOG_PATH"))
     }
+		
+		# login to hublot
+		clessnverse::logit(scriptname, "connecting to hub", logger)
+
+		# connect to hublot
+		credentials <<- hublot::get_credentials(
+				Sys.getenv("HUB3_URL"), 
+				Sys.getenv("HUB3_USERNAME"), 
+				Sys.getenv("HUB3_PASSWORD"))
+		
+		# or connect to hub2
+		#clessnhub::login(
+		#    Sys.getenv("HUB_USERNAME"),
+		#    Sys.getenv("HUB_PASSWORD"),
+		#    Sys.getenv("HUB_URL"))
+
+		clessnverse::logit(scriptname, paste("Execution of",  scriptname,"starting"), logger)
+		
+		main()
+	},
+
+	warning = function(w) {
+		clessnverse::logit(scriptname, paste(w, collapse=' '), logger)
+		print(w)
+		final_message <<- if (final_message == "") w else paste(final_message, "\n", w, sep="")    
+		status <<- 2
+	}),
+    
+	error = function(e) {
+		clessnverse::logit(scriptname, paste(e, collapse=' '), logger)
+		print(e)
+		final_message <<- if (final_message == "") e else paste(final_message, "\n", e, sep="")    
+		status <<- 1
+	},
+  
+	finally={
+		clessnverse::logit(scriptname, final_message, logger)
+
+		clessnverse::logit(scriptname, 
+			paste(
+					some_variable_to_count_what_the_script_does, 
+					"some message to say what it has done"
+			),
+			logger
+		)
+
+		clessnverse::logit(scriptname, paste("Execution of",  scriptname,"program terminated"), logger)
+		clessnverse::log_close(logger)
+		if exists(logger) rm(logger)
+		print(paste("exiting with status", status))
+		quit(status = status)
+	}
 )
-
-
