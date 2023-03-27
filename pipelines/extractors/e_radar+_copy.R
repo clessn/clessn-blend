3489015552869fd6efabd6c393c3843979b7340e
ClementCadieux <cadieuxclement@gmail.com>
2023-03-27 18:06:40 +0000

    Changer le nom de test pour être plus clair

--- pipelines/extractors/e_radar+_copy.R
+++ pipelines/extractors/e_radar+_copy.R
@@ -54,45 +54,45 @@ harvest_headline <- function(r, m) {
   found_supported_media <- FALSE
 
   if (m$short_name == "RCI") {
-    test <- r %>% rvest::html_nodes(xpath = '//*[@class="item--1"]') %>% rvest::html_nodes("a") %>% rvest::html_attr("href")
-    url <- paste(m$base, test[[1]], sep="")
+    RCI_extracted_headline <- r %>% rvest::html_nodes(xpath = '//*[@class="item--1"]') %>% rvest::html_nodes("a") %>% rvest::html_attr("href")
+    url <- paste(m$base, RCI_extracted_headline[[1]], sep="")
     found_supported_media <- TRUE
   }
 
   if (m$short_name == "JDM") {
-    test <- r %>%
+    JDM_extracted_headline <- r %>%
       rvest::html_elements(xpath = "//a") %>% 
       rvest::html_element("span") %>% 
       rvest::html_attr("data-story-url") %>%
       na.omit()
-    url <- test[[1]]
+    url <- JDM_extracted_headline[[1]]
     found_supported_media <- TRUE
   }
 
   if (m$short_name == "CBC") {
-    test <<- r %>%
+    CBC_extracted_headline <<- r %>%
       #rvest::html_nodes(xpath = '//*[@class="primaryHeadlineLink sclt-contentpackageheadline"]') %>% 
       rvest::html_nodes(xpath = '//div[@class="card cardFeatured cardFeaturedReversed sclt-featurednewsprimarytopstoriescontentlistcard0"]') %>%
       rvest::html_node("a") %>%
       rvest::html_attr("href")
 
     if (length(test) == 0) {
-      test <<- r %>%
+      CBC_extracted_headline <<- r %>%
         rvest::html_nodes(xpath = '//a[@class="primaryHeadlineLink sclt-contentpackageheadline"]') %>% 
         rvest::html_attr("href")
     }
 
     if (grepl("^http.*", test[[1]])) {
-      url <- test[[1]]
+      url <- CBC_extracted_headline[[1]]
     } else {
-      url <- paste(m$base, test[[1]], sep="")
+      url <- paste(m$base, CBC_extracted_headline[[1]], sep="")
     } 
     found_supported_media <- TRUE
   }
 
   if(m$short_name == "TVA"){
-    test <- r %>% rvest::html_nodes(xpath = '//*[@class="home-top-story"]') %>% rvest::html_nodes(xpath = '//*[@class="news_unit_link"]') %>% rvest::html_attr("href")
-    url <- paste(m$base, test[[1]], sep="")
+    TVA_extracted_headline <- r %>% rvest::html_nodes(xpath = '//*[@class="home-top-story"]') %>% rvest::html_nodes(xpath = '//*[@class="news_unit_link"]') %>% rvest::html_attr("href")
+    url <- paste(m$base, TVA_extracted_headline[[1]], sep="")
     found_supported_media <- TRUE
   }
 
