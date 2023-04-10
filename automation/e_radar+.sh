c5cb9314a078ae399274b0088d31dd99a43317c0
ClementCadieux <cadieuxclement@gmail.com>
2023-04-10 13:30:51 +0000

    Créer le sh pour e_radar+

---
+++ automation/e_radar+.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+generate_post_data()
+{
+  cat <<EOF
+  {
+   "text": "\n${status}: ${scriptname} ${output_msg} on $(date)\n${output}\n"
+  }
+EOF
+}
+
+scriptname="e_radar+"
+
+R --no-save --no-restore -e 'install.packages("remotes", repos = "http://cran.us.r-project.org")'
+R --no-save --no-restore -e 'remotes::install_github("clessn/clessnverse", force=T, upgrade="never")'
+
+cd ~
+
+Rscript --no-save --no-restore $CLESSN_ROOT_DIR/clessn-blend/pipelines/extractors/$scriptname.R 2>&1
+ret=$?
+
+sed 's/\"/\\"/g' -i ~/logs/$scriptname.log
+sed 's/^M//g ' -i ~/logs/$scriptname.log
+
+if [ $ret -eq 0 ]; then
+  status="SUCCESS"
+  output_msg="completed successfully"
+fi
+
+if [ $ret -eq 1 ]; then
+  status="ERROR"
+  output_msg="generated one or more errors"
+fi
+
+if [ $ret -eq 2 ]; then
+  status="WARNING"
+  output_msg="generated one or more warnings"
+fi
+
+if [ $ret -ne 0 ]; then
+  output=`tail -n 10 ~/logs/$scriptname.log`
+  curl -X POST -H 'Content-type: application/json' --data "$(generate_post_data)" https://hooks.slack.com/services/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
+else
+  output=`tail -n 7 ~/logs/$scriptname.log`
+  curl -X POST -H 'Content-type: application/json' --data "$(generate_post_data)" https://hooks.slack.com/services/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
+fi
