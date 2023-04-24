1afc6a8ebc1a0f4aad551fac803ad9e7027c7736
ClementCadieux <cadieuxclement@gmail.com>
2023-04-24 18:40:49 +0000

    GAM Duplication

--- pipelines/extractors/e_radar+.R
+++ pipelines/extractors/e_radar+.R
@@ -373,6 +373,18 @@ find_headline <- function(r, m){
 }
 
 get_content <- function(r, m){
+  if(m$short_name == "GAM"){
+    article_content <<- r %>%
+      rvest::html_nodes(xpath = '//article[@class="l-article"]') %>%
+      rvest::html_text()
+
+    if(length(article_content) > 0){
+      return(article_content[[1]])
+    } else {
+      clessnverse::logit(scriptname, paste(m$short_name, ": Empty content", sep = ""), logger)
+    }
+  }
+
   if(m$short_name == "VS"){
     article_content <<- r %>%
       rvest::html_nodes(xpath = '//article[@class="article-content-story article-content-story--story"]') %>%
