0830792c6cfd81d2d654df356ea57476981d4c26
p2xcode <p2x@p2xco.de>
2023-11-30 13:05:24 +0000

    Remove unnecessary code in ca_get_press_releases.R

--- utils/agora+ca/ca_get_press_releases.R
+++ utils/agora+ca/ca_get_press_releases.R
@@ -3,20 +3,6 @@ clessnhub::login(
    Sys.getenv("HUB_PASSWORD"),
    Sys.getenv("HUB_URL"))
 
-
-my_filter <- clessnhub::create_filter(
-  type="parliament_debate", 
-  schema="v2", 
-  metadata=list(
-    institution="House of Commons of Canada"), 
-    #format="xml"),
-  data=list(
-    #eventID="432123HAN123"
-    #eventDate__gte="2023-01-01"
-    eventDate__lte="2007-01-29"
-  )
-)
-
 my_filter <- clessnhub::create_filter()
 
 df <- clessnhub::get_items(
